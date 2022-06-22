8bdf1156be8b0aff3d8e84136b15508bfdba1415
Thomas <malifarge.thomas9@gmail.com>
2022-06-22 14:16:46 +0200

    Nombre impairs

--- index.js
+++ index.js
@@ -12,3 +12,17 @@ for ( let i = 11; i <47; i++){
 }
 
 console.log(somme);
+
+// 2 - Nombre impairs
+
+let sommeimpaire = 0
+
+for ( let i = 102; i <588; i++){
+    if (i%2===1){
+        console.log(i);
+        sommeimpaire += i
+    }
+
+}
+
+console.log(sommeimpaire);
