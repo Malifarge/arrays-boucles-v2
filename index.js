96101b1d0e8a2224d38e8ad4e4b77f69f5546c8d
Thomas <malifarge.thomas9@gmail.com>
2022-06-22 14:24:43 +0200

    Puissance et racine carrée

--- index.js
+++ index.js
@@ -15,14 +15,32 @@ console.log(somme);
 
 // 2 - Nombre impairs
 
-let sommeimpaire = 0
+let sommeImpaire = 0
 
 for ( let i = 102; i <588; i++){
     if (i%2===1){
         console.log(i);
-        sommeimpaire += i
+        sommeImpaire += i
     }
 
 }
 
-console.log(sommeimpaire);
+console.log(sommeImpaire);
+
+// 3 - Puissance et racine carrée
+
+const numbers = [1, 2, 3, 4, 5]
+const squares = []
+const roots = []
+
+for (i=0; i < numbers.length;i++){
+    squares[i] = numbers[i]**2
+}
+
+for (i=0; i < squares.length;i++){
+    roots[i] = Math.sqrt(squares[i])
+}
+
+console.log(numbers);
+console.log(squares);
+console.log(roots);
